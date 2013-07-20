c4a7c59b17540250c3f392caf6e0da4753559087
altersaddle <malcolm@ipatch.ca>
2013-07-20 10:22:25 -0700

    SQL to generate databases and tables

---
+++ Database/tables.sql
@@ -0,0 +1,39 @@
+USE [garden]
+GO
+
+/****** Object:  Table [dbo].[SENSOR_DESC]    Script Date: 7/20/2013 10:15:56 AM ******/
+DROP TABLE [dbo].[SENSOR_DESC]
+GO
+/****** Object:  Table [dbo].[SENSORDATA]    Script Date: 7/20/2013 10:15:28 AM ******/
+DROP TABLE [dbo].[SENSORDATA]
+GO
+
+/****** Object:  Table [dbo].[SENSOR_DESC]    Script Date: 7/20/2013 10:15:56 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[SENSOR_DESC](
+	[ID] [numeric](18, 0) IDENTITY(1,1) NOT NULL,
+	[SENSOR] [nchar](16) NOT NULL,
+	[DESCRIPTION] [nchar](255) NULL
+) ON [PRIMARY]
+
+GO
+
+/****** Object:  Table [dbo].[SENSORDATA]    Script Date: 7/20/2013 10:15:28 AM ******/
+CREATE TABLE [dbo].[SENSORDATA](
+	[ID] [numeric](18, 0) IDENTITY(1,1) NOT NULL,
+	[DEVICE] [nchar](16) NOT NULL,
+	[SENSOR] [nchar](16) NOT NULL,
+	[NUMVAL] [numeric](18, 0) NULL,
+	[STRVAL] [nchar](255) NULL,
+	[OBSERVED] [datetime] NULL,
+	[COLLECTED] [datetime] NULL
+) ON [PRIMARY]
+
+GO
+
+
