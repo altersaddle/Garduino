8a1093ee0268cd6ae7fd9781a31b99a401a577ee
altersaddle <malcolm@ipatch.ca>
2013-08-10 10:39:21 -0700

    30 day averages!

--- SerialMonitor/GenerateJSON/GenerateJSON.cs
+++ SerialMonitor/GenerateJSON/GenerateJSON.cs
@@ -28,7 +28,8 @@ namespace GenerateJSON
     {
         // TODO: Fetch DB connection from common storage
         static string connectionStringTemplate = "Server=192.168.1.32;Database=garden;User Id={0};Password={1}";
-        static string past24HourQuery = "SELECT TOP 96 AVG(NUMVAL) AS TEMPERATURE, STEP FROM SENSOR_AGG WHERE DEVICE = @DEVICE AND SENSOR = @SENSOR GROUP BY STEP ORDER BY MAX(COLLECTED) DESC";
+        static string past24HourQuery = "SELECT TOP 96 AVG(NUMVAL) AS VALUE, STEP FROM SENSOR_AGG WHERE DEVICE = @DEVICE AND SENSOR = @SENSOR GROUP BY STEP ORDER BY MAX(COLLECTED) DESC";
+        static string past30DayQuery = "SELECT TOP 30 MIN(NUMVAL) AS MINVAL, MAX(NUMVAL) AS MAXVAL,	AVG(NUMVAL) AS AVGVAL, CONVERT(date,COLLECTED) AS DATE FROM SENSORDATA WHERE DEVICE = @DEVICE AND SENSOR = @SENSOR GROUP BY CONVERT(date,COLLECTED) ORDER BY MAX(COLLECTED) DESC";
 
         static void Main(string[] args)
         {
@@ -45,6 +46,10 @@ namespace GenerateJSON
             Console.WriteLine(past24Temp);
             string past24Humidity = app.generatePast24Hours("GARDEN1", "DHT220", connectionString, "humidityData");
             Console.WriteLine(past24Humidity);
+            string monthTemp = app.generate30DaySummary("GARDEN1", "DHT221", connectionString, "temp30dayData");
+            Console.WriteLine(monthTemp);
+            string monthHumidity = app.generate30DaySummary("GARDEN1", "DHT220", connectionString, "humidity30dayData");
+            Console.WriteLine(monthHumidity);
         }
 
         private string generatePast24Hours(string device, string sensor, string connectionString, string variableName)
@@ -86,6 +91,96 @@ namespace GenerateJSON
                 }
 
                 retVal = string.Format("var {2} = {{ labels : [\"{1}\"], datasets : [{{data : [{0}]}}] }};", string.Join(",", data), string.Join("\",\"", labels), variableName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                try
+                {
+                    if (conn != null)
+                    {
+                        conn.Close();
+                    }
+                }
+                catch (Exception)
+                {
+                    // It's over, all over
+                }
+            }
+            return retVal;
+        }
+
+        private string generate30DaySummary(string device, string sensor, string connectionString, string variableName)
+        {
+            SqlConnection conn = null;
+            string[] labels = null;
+            string[] mindata = null;
+            string[] maxdata = null;
+            string[] avgdata = null;
+            string retVal = string.Empty;
+
+            try
+            {
+                using (conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    List<string> labelList = new List<string>();
+                    List<string> minValueList = new List<string>();
+                    List<string> maxValueList = new List<string>();
+                    List<string> avgValueList = new List<string>();
+
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.CommandText = past30DayQuery;
+
+                        cmd.Parameters.Add(new SqlParameter("@DEVICE", device));
+                        cmd.Parameters.Add(new SqlParameter("@SENSOR", sensor));
+
+                        using (SqlDataReader results = cmd.ExecuteReader())
+                        {
+                            while (results.Read())
+                            {
+                                minValueList.Add(string.Format("{0}", results[0]));
+                                maxValueList.Add(string.Format("{0}", results[1]));
+                                avgValueList.Add(string.Format("{0}", results[2]));
+                                labelList.Add(string.Format("{0}", results[3]));
+                            }
+                        }
+                    }
+
+                    while (labelList.Count < 30)
+                    {
+                        // pad
+                        minValueList.Add(string.Empty);
+                        maxValueList.Add(string.Empty);
+                        avgValueList.Add(string.Empty);
+                        labelList.Add(string.Empty);
+                    }
+
+                    labels = labelList.Reverse<string>().ToArray();
+                    mindata = minValueList.Reverse<string>().ToArray();
+                    maxdata = maxValueList.Reverse<string>().ToArray();
+                    avgdata = avgValueList.Reverse<string>().ToArray();
+                }
+
+                retVal = string.Format("var {0} = {{ labels : [\"{1}\"], datasets : [{{fillColor:\"rgba({2},0.33)\",strokeColor:\"rgba({2},1)\",data:[{3}]}}," +
+                    "{{fillColor:\"rgba({4},0.5)\",strokeColor:\"rgba({4},1)\",data:[{5}]}}," +
+                    "{{fillColor:\"rgba({6},0.5)\",strokeColor:\"rgba({6},1)\",data:[{7}]}}] }};",
+                    variableName,
+                    string.Join("\",\"", labels),
+                    "151,187,205",
+                    string.Join(",", mindata),
+                    "220,220,220",
+                    string.Join(",", avgdata),
+                    "205,187,151",
+                    string.Join(",", maxdata)
+                    );
             }
             catch (Exception e)
             {
